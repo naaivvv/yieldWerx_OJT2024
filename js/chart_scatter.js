e40409b7c151b526b94329e836aa9c579e027c6d
Winston Navales Barsabal <winston.barsabal@gmail.com>
2024-08-21 20:45:59 +0800

    scatter point radius reduce

--- js/chart_scatter.js
+++ js/chart_scatter.js
@@ -114,7 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     backgroundColor: 'rgba(75, 192, 192, 0.6)',
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1,
-                    pointRadius: 3,
+                    pointRadius: 1,
                     spanGaps: true // enable for a single dataset
                 }]
             },
